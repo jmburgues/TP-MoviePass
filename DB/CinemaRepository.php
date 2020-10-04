66c22564b25c539609f2b28ff41ae8388ac77a0d
Juan Manuel Burgues Schaab <securetux@gmail.com>
2020-10-04 17:50:10 -0300

    CinemaRepo y Dao

---
+++ DB/CinemaRepository.php
@@ -0,0 +1,68 @@
+<?php namespace Data; // PUEDE QUE ESTE REPOSITORIO LUEGO SEA MODIFICADO POR UNA BASE DE DATOS
+
+use Models\Cinema as Cinema;
+
+class CinemaRepository {
+    private $cinemasList = array();
+    private $fileName;
+
+    public function __construct(){
+        $this->fileName = dirname(__DIR__) . "/data/cinemas.json";
+    }
+
+    public function Add($cinemaObject){
+        $this->RetrieveData();
+
+        array_push($this->cinemasList,$cinemaObject);
+
+        $this->SaveData();
+    }
+
+    public function GetAll(){
+        $this->RetrieveData();
+
+        return $this->cinemasList;
+    }
+
+    private function SaveData()
+    {
+        $arrayToEncode = array();
+
+        foreach($this->cinemasList as $cinemaObject)
+        {
+            $valuesArray["name"] = $cinemaObject->getName();
+            $valuesArray["adress"] = $cinemaObject->getAdress();
+            $valuesArray["opening"] = $cinemaObject->getOpenning();
+            $valuesArray["closing"] = $cinemaObject->getClosing();
+            $valuesArray["ticketValue"] = $cinemaObject->getTicketValue();
+            $valuesArray["active"] = $cinemaObject->getActive();
+
+            array_push($arrayToEncode, $valuesArray);
+        }
+
+        $jsonContent = json_encode($arrayToEncode, JSON_PRETTY_PRINT);
+        
+        file_put_contents($this->fileName, $jsonContent);
+    }
+
+    private function RetrieveData()
+    {
+        $this->cinemasList = array();
+
+        if(file_exists($this->fileName))
+        {
+            $jsonContent = file_get_contents($this->fileName);
+
+            $arrayToDecode = ($jsonContent) ? json_decode($jsonContent, true) : array();
+
+            foreach($arrayToDecode as $valuesArray)
+            {
+                $cinemaObject = new Cinema($valuesArray["name"], $valuesArray["adress"], 
+                                            $valuesArray["opening"], $valuesArray["closing"], 
+                                            $valuesArray["ticketValue"], $valuesArray["active"]);
+                
+                array_push($this->cinemasList, $cinemaObject);
+            }
+        }
+    }
+}
---
+++ Dao/Cinemas.php
@@ -0,0 +1,16 @@
+<?php namespace Dao;
+
+use Data\CinemaRepository;
+use DB\CinemaRepository as cineRepo;
+
+class Cienmas {
+
+    public function read(){
+
+        $cineRepo = new CinemaRepository();
+        $cineRepo->rea
+
+    }
+}
+
+?>
