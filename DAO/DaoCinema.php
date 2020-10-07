5d768d17e20fb4e5bfa6ef785207a5578a25926c
Juan Manuel Burgues Schaab <securetux@gmail.com>
2020-10-07 16:52:40 -0300

    cambios

---
+++ DAO/DaoCinema.php
@@ -0,0 +1,78 @@
+<?php namespace DAO; // PUEDE QUE ESTE REPOSITORIO LUEGO SEA MODIFICADO POR UNA BASE DE DATOS
+
+use Models\Cinema as Cinema;
+
+class DaoCinema {
+    private $cinemasList = array();
+    private $fileName;
+
+    public function __construct(){
+        $this->fileName = dirname(__DIR__) . "/data/cinemas.json";
+    }
+
+    public function Add($cinemaObject){
+        $this->RetrieveData();
+
+        array_push($this->cinemasList,$cinemaObject);
+
+        $this->SaveData();
+    }
+
+    public function Remove($cinemaObject){
+        $this->RetrieveData();
+
+        $objectKey = array_search($cinemaObject,$this->cinemasList,true);
+        unset($this->cinemasList[$objectKey]);
+
+        // Hay que decidir si borrar el elemento o modificar el valor "active" a false.
+        // si se modifica a false, hay que repensar el Add.
+    }
+
+    public function GetAll(){
+        $this->RetrieveData();
+
+        return $this->cinemasList;
+    }
+
+    private function SaveData()
+    {
+        $arrayToEncode = array();
+
+        foreach($this->cinemasList as $cinemaObject)
+        {
+            $valuesArray["name"] = $cinemaObject->getName();
+            $valuesArray["adress"] = $cinemaObject->getAdress();
+            $valuesArray["opening"] = $cinemaObject->getOpenning();
+            $valuesArray["closing"] = $cinemaObject->getClosing();
+            $valuesArray["ticketValue"] = $cinemaObject->getTicketValue();
+            $valuesArray["active"] = $cinemaObject->getActive();
+
+            array_push($arrayToEncode, $valuesArray);
+        }
+
+        $jsonContent = json_encode($arrayToEncode, JSON_PRETTY_PRINT);
+        
+        file_put_contents($this->fileName, $jsonContent);
+    }
+
+    private function RetrieveData()
+    {
+        $this->cinemasList = array();
+
+        if(file_exists($this->fileName))
+        {
+            $jsonContent = file_get_contents($this->fileName);
+
+            $arrayToDecode = ($jsonContent) ? json_decode($jsonContent, true) : array();
+
+            foreach($arrayToDecode as $valuesArray)
+            {
+                $cinemaObject = new Cinema($valuesArray["name"], $valuesArray["adress"], 
+                                            $valuesArray["opening"], $valuesArray["closing"], 
+                                            $valuesArray["ticketValue"], $valuesArray["active"]);
+                
+                array_push($this->cinemasList, $cinemaObject);
+            }
+        }
+    }
+}
--- DB/CinemaRepository.php
+++ DB/CinemaRepository.php
@@ -1,78 +1,57 @@
-<?php namespace Data; // PUEDE QUE ESTE REPOSITORIO LUEGO SEA MODIFICADO POR UNA BASE DE DATOS
+<?php namespace DB;
 
-use Models\Cinema as Cinema;
 
 class CinemaRepository {
-    private $cinemasList = array();
-    private $fileName;
 
-    public function __construct(){
-        $this->fileName = dirname(__DIR__) . "/data/cinemas.json";
-    }
-
-    public function Add($cinemaObject){
-        $this->RetrieveData();
-
-        array_push($this->cinemasList,$cinemaObject);
-
-        $this->SaveData();
-    }
-
-    public function Remove($cinemaObject){
-        $this->RetrieveData();
-
-        $objectKey = array_search($cinemaObject,$this->cinemasList,true);
-        unset($this->cinemasList[$objectKey]);
-
-        // Hay que decidir si borrar el elemento o modificar el valor "active" a false.
-        // si se modifica a false, hay que repensar el Add.
-    }
-
-    public function GetAll(){
-        $this->RetrieveData();
-
-        return $this->cinemasList;
-    }
-
-    private function SaveData()
-    {
-        $arrayToEncode = array();
-
-        foreach($this->cinemasList as $cinemaObject)
-        {
-            $valuesArray["name"] = $cinemaObject->getName();
-            $valuesArray["adress"] = $cinemaObject->getAdress();
-            $valuesArray["opening"] = $cinemaObject->getOpenning();
-            $valuesArray["closing"] = $cinemaObject->getClosing();
-            $valuesArray["ticketValue"] = $cinemaObject->getTicketValue();
-            $valuesArray["active"] = $cinemaObject->getActive();
-
-            array_push($arrayToEncode, $valuesArray);
+    public function getAll(){
+        $cineRepo = new CinemaRepository();
+        $cinemasList = $cineRepo->GetAll();
+        
+        function comparador($a,$b){
+            return $a->getId() > $b->getId();
         }
 
-        $jsonContent = json_encode($arrayToEncode, JSON_PRETTY_PRINT);
+        usort($cinemasList,'comparador');
         
-        file_put_contents($this->fileName, $jsonContent);
+        return $cinemasList;
     }
 
-    private function RetrieveData()
-    {
-        $this->cinemasList = array();
+    public function add($newCinema){
+        $cineRepo = new CinemaRepository();
 
-        if(file_exists($this->fileName))
-        {
-            $jsonContent = file_get_contents($this->fileName);
+        $cinemasList = $cineRepo->GetAll();
 
-            $arrayToDecode = ($jsonContent) ? json_decode($jsonContent, true) : array();
-
-            foreach($arrayToDecode as $valuesArray)
-            {
-                $cinemaObject = new Cinema($valuesArray["name"], $valuesArray["adress"], 
-                                            $valuesArray["opening"], $valuesArray["closing"], 
-                                            $valuesArray["ticketValue"], $valuesArray["active"]);
-                
-                array_push($this->cinemasList, $cinemaObject);
+        foreach ($cinemasList as $existentCinema){
+            if($newCinema->getName() == $existentCinema->getName()){
+                return false;
             }
         }
+
+        $cineRepo->Add($newCinema);
+
+        return true;
+    }
+
+    public function remove($cinemaObject){
+        $cineRepo = new CinemaRepository();
+        $cineRepo->Remove($cinemaObject);
+    }
+
+    public function update($modifiedCinema){
+        $cineRepo = new CinemaRepository();
+
+        $cinemasList = $cineRepo->GetAll();
+
+        foreach ($cinemasList as $existentCinema){
+            if($modifiedCinema->getId() == $existentCinema->getId()){
+                $cineRepo->Remove($existentCinema);
+                $cineRepo->Add($modifiedCinema);
+                
+                return true;
+            }
+        }
+        return false;        
     }
 }
+
+?>
