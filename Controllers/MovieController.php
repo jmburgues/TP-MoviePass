479093c20ec63c1e54fa4dd629060b2b1aa12868
LautaroBG <48892699+LautaroBG@users.noreply.github.com>
2020-10-08 14:01:21 -0300

    fixing some small errors

--- Controllers/MovieController.php
+++ Controllers/MovieController.php
@@ -12,12 +12,12 @@ use DateTime;
 class MovieController{
   
   function getLatestMoviesFromApi(){  
-    $data = file_get_contents("http://api.themoviedb.org/3/movie/now_playing?page=1&language=en-US&api_key=601e12bf1e7197e7532eb9c4901b0d3a";
+    $data = file_get_contents("http://api.themoviedb.org/3/movie/now_playing?page=1&language=en-US&api_key=601e12bf1e7197e7532eb9c4901b0d3a");
     $array = ($data) ? json_decode($data, true) : array();
     $result = $array["results"];
     foreach ($result as $value){
       if(is_array($value)){
-        $movieData = file_get_contents("https://api.themoviedb.org/3/movie/".$value["id"]"?language=en-US&api_key=601e12bf1e7197e7532eb9c4901b0d3a");
+        $movieData = file_get_contents("https://api.themoviedb.org/3/movie/".$value["id"]."?language=en-US&api_key=601e12bf1e7197e7532eb9c4901b0d3a");
         $movie = ($movieData) ? json_decode($movieData, true) : array();
         $genre = array();
         foreach ($value["genres"] as $genreData) {
