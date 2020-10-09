d52548878f2b67ff0255cbaee4e2f4850d328baa
LautaroBG <48892699+LautaroBG@users.noreply.github.com>
2020-10-09 20:31:51 -0300

    ajustes de nombres

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -6,7 +6,7 @@
         public function Index($message = "")
         {
         
-            require_once(VIEWS_PATH."index.php");
+            require_once(VIEWS_PATH."home.php");
         }        
     }
 ?>
