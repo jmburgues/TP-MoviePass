66d1659ccbab02100f1358fdb9ff36c0ac002710
Juan Manuel Burgues Schaab <securetux@gmail.com>
2020-10-14 21:23:50 -0300

    cambios modify cinema

--- Controllers/CinemaController.php
+++ Controllers/CinemaController.php
@@ -53,34 +53,34 @@
       }
     }
 
-      public function AddCinema($name, $address, $number, $openning, $closing, $ticketValue ){
-        $cinema = new Cinema();
-        $cinema->setName($name);
-        $cinema->setAddress($address);
-        $cinema->setNumber($number);
-        $cinema->setOpenning($openning);
-        $cinema->setClosing($closing);
-        $cinema->setTicketValue($ticketValue);
-        $list=$this->DAOCinema->GetAll();  
-      //  $flag = false;
-
-        //Control del refresh del form
-        foreach($list as $l){
-          if($l->getName() != $cinema->getName() ){
-        //    $flag = true;
-        if ($cinema->getName() != ""  ) {
-                $this->DAOCinema->Add($cinema);
-                $message = "Cinema successfully added";
-            } else {
-                $message = "Cinema already added";
-              }
-              echo "<script type='text/javascript'>alert('$message');</script>";
-              $cinemas = $this->DAOCinema->getActiveCinemas();  
-              include VIEWS_PATH.'adminView.php';
-        }
+    public function AddCinema($name, $address, $number, $openning, $closing, $ticketValue ){
+      $cinema = new Cinema();
+      $cinema->setName($name);
+      $cinema->setAddress($address);
+      $cinema->setNumber($number);
+      $cinema->setOpenning($openning);
+      $cinema->setClosing($closing);
+      $cinema->setTicketValue($ticketValue);
+      $list=$this->DAOCinema->GetAll();  
+      
+      //Control del refresh del form
+      if($name != ""){ // validaciones de nombre
+        foreach($list as $l){   
+          if($l->getName() == $name){
+            if(!$l->getActive()){ // verifico borrado logico
+              $this->DAOCinema->modify($cinema);
+              $message = "The cinema is now active again.";
+            }
+          }
+          else{ // si no hay cines con mismo nombre, agrego.
+            $this->DAOCinema->Add($cinema);
+            $message = "Cinema successfully added";
           }
         }
-
-        //Control de un cine ya existente
+      }
+      echo "<script type='text/javascript'>alert('$message');</script>";
+      $cinemas = $this->DAOCinema->getActiveCinemas(); 
+      include VIEWS_PATH.'adminView.php';
+    }
   }
 ?>
