b1f7c77cea6009bcffb349e166a771d79c07a710
Juan Manuel Burgues Schaab <securetux@gmail.com>
2020-12-01 14:04:31 -0300

    Solves user auth bug

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -14,6 +14,9 @@ namespace Controllers;
                 else if($accessRole == 'admin' && ($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'owner')){
                     $auth = true;
                 }
+                else if($accessRole == 'user'){
+                    $auth = true;
+                }
             }
             if(!$auth){
                 ViewController::errorView("ERROR 403: Access denied.");
